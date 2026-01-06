48047cf55d554f13012ff8a4b1c1b92689757b1d
Maty Janda <matous.janda@atlas.cz>
2026-01-06 20:27:37 +0100

    Build fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import { GoogleTagManager } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
